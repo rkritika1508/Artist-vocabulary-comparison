1de06389cab7fac666989010f6635635a08c9cb1
Kritika <rkritika1508@gmail.com>
2018-05-28 01:48:01 +0530

    Trying to scrape wikipedia table

--- scrape.py
+++ scrape.py
@@ -1,44 +1,33 @@
 import urllib.request as ureq
+import wikipedia
 from bs4 import BeautifulSoup as soup
 import re
 
-my_url = "https://www.imdb.com/list/ls051517906/"
+my_url = "https://en.wikipedia.org/wiki/List_of_best-selling_music_artists"
 
 uclient = ureq.urlopen(my_url)
 page_html = uclient.read()
 uclient.close()
 page_soup = soup (page_html, "html.parser")
-containers = page_soup.findAll("h3", {"class":"lister-item-header"})
+#containers = page_soup.findAll("div", {"class":""})
+containers = page_soup.findAll("th",{"scope":"row"})
 artists = []
 artists_links = []
-num = 0
+
 for container in containers:
-    container = container.a.text.replace("\n", "")[1:]
-    artists.append(container)
-    num = num+1
-artists[11] = "Beach Boys"
-artists[39] = "Simon Garfunkel"
-artists[47] = "Run DMC"
-artists[53] = "Howlin Wolf"
-artists[96] = "R E M"
-#Bo Diddley doesn't have any song in the AZ lyrics collection.
-#So, he will have to be removed from the artists list.
-artists.remove('Bo Diddley')
-
-#Booker T and the MGs were an instrumental group.
-#So, they must b removed too.
-artists.remove("Booker T. & the M.G.s")
-
-regex = re.compile('[^a-zA-Z0-9]')
+    artists.append(container.a.text)
+    #artists.append(container)
+#    num = num+1
+print(artists)
     
-for artist in artists:
-    artist_search = artist.replace(" ", "+")
-    lyrics_url = "https://search.azlyrics.com/search.php?q=" + artist_search
-    x = ureq.urlopen(lyrics_url)
-    x_html = x.read()
-    x.close()
-    x_soup = soup(x_html, "html.parser")
-    cts = x_soup.td.a["href"]
+#for artist in artists:
+#    artist_search = artist.replace(" ", "+")
+#    lyrics_url = "https://search.azlyrics.com/search.php?q=" + artist_search
+#    x = ureq.urlopen(lyrics_url)
+#    x_html = x.read()
+#    x.close()
+#    x_soup = soup(x_html, "html.parser")
+#    cts = x_soup.td.a["href"]
     #artists_links.append(cts)
-    print(cts, artist)
+#    print(cts, artist)
 
