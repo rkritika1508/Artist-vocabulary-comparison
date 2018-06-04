205ed3898acdfaf366cd96df7540a6b05b4eebdc
Kritika <rkritika1508@gmail.com>
2018-06-04 23:48:43 +0530

    Song Lyrics scraped from azlyrics

---
+++ Artists Lyrics.py
@@ -0,0 +1,68 @@
+import urllib.request as ureq
+from bs4 import BeautifulSoup as soup
+import pandas as pd
+import random
+import requests
+import sys
+from time import sleep
+
+data = pd.read_csv('Final_Song_List.csv')
+songs_link = []
+lyrics_list = []
+
+for i in range(0, 23359):
+    link_new = "http://azlyrics.com" + data['Songs_list'][i][2:]
+    # azlyrics is clever! it knows im a bot!
+    user_agent_list = [
+       #Chrome
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 5.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36',
+        #Firefox
+        'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 6.2; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.3; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)',
+        'Mozilla/5.0 (Windows NT 6.1; Win64; x64; Trident/7.0; rv:11.0) like Gecko',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Trident/6.0)',
+        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 5.1; Trident/4.0; .NET CLR 2.0.50727; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729)'
+    ]
+
+    user_agent = random.choice(user_agent_list)
+    headers = {'User-Agent': user_agent}
+
+    res = requests.get(link_new, headers=headers)
+    # raise if there is an issue
+    try:
+        res.raise_for_status()
+    except Exception as exc:
+        print(exc) # print error
+        sys.exit() # end program on error
+
+    print(i, 'connected to %s' % link_new)
+
+    # initiate BeautifulSoup
+    site = soup(res.text, "html.parser")
+    sleep(random.randint(3, 5))
+    # find the blank div that contains the lyrics
+    for lyrics in site.find_all("div", {"class": ""}):
+        lyrics_list.append(lyrics.text)
+        songs_link.append(link_new)
+
+lyrics_data = {'Songs_link': songs_link, 'Lyrics': lyrics_list}
+df = pd.DataFrame(lyrics_data)
+df.to_csv("SongLyrics.csv")
